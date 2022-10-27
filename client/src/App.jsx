109576f0194c32239083fbd6eeab13dc07fd608b
AllegriM <elmonky99@gmail.com>
2022-10-27 19:51:11 -0300

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Landing from './components/Landing'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Landing />} />{' '}
+        <Route exact path="/" element={<Landing />} />{" "}
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <App />
   </Router>
-)
+);
