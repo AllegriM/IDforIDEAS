05c85899e5eb90809b9e600f162b5f1d3ae4ede6
unknown <miguel00@hotmail.com>
2022-10-27 16:57:05 -0500

    controllers and routes reoganization for implements back documentation

--- api/src/routes/Product.js
+++ api/src/routes/Product.js
@@ -1,78 +1,12 @@
 const { Router } = require("express");
-const { Product } = require("../db");
+const {  getAllProducts,findProduct,createProduct,editProduct,deleteProduct} = require('../controllers/Product')
 
 const router = Router();
 
-router.get("/all", async (req, res, next) => {
-  try {
-    const allProducts = await Product.findAll();
-    return res.status(200).json({ result: allProducts });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/view/:idProduct", async (req, res, next) => {
-  let { idProduct } = req.params;
-  try {
-    const productFound = await Product.findByPk(idProduct);
-    if (!productFound) {
-      return res.status(404).json({ result: "Product Not Found" });
-    }
-    return res.status(200).json({ result: productFound });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post("/create", async (req, res, next) => {
-  const { name, description, price, stock } = req.body;
-
-  try {
-    const newProduct = await Product.create({
-      name,
-      description,
-      price,
-      stock,
-    });
-    return res.status(201).json({ result: newProduct });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/edit/:idProduct", async (req, res, next) => {
-  const { idProduct } = req.params;
-  const { stock, name, description, price } = req.body;
-
-  try {
-    let productFound = await Product.findByPk(idProduct);
-    if (productFound) {
-      await productFound.set({ stock, name, description, price });
-      productFound.save();
-      return res.json({ result: productFound });
-    } else {
-      return res.json({ result: "Product Not Found" });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/delete/:idProduct", async (req, res, next) => {
-  const { idProduct } = req.params;
-
-  try {
-    const productFound = await Product.findByPk(idProduct);
-    if (productFound) {
-      await productFound.destroy();
-      return res.json({ result: "Product Deleted" });
-    } else {
-      return res.json({ result: "Product Not Found" });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/all", getAllProducts)
+router.get("/view/:idProduct",findProduct)
+router.post("/create",createProduct)
+router.put("/edit/:idProduct",editProduct)
+router.delete("/delete/:idProduct",deleteProduct)
 
 module.exports = router;
--- api/src/routes/User.js
+++ api/src/routes/User.js
@@ -1,40 +1,15 @@
-const bcrypt = require("bcrypt");
 const { Router } = require("express");
-const { User } = require("../db");
-const { generateAccessToken } = require("../Controllers/User");
-
+const {
+  registerUser,
+  loginUser,
+  resetPassword,
+  getAllUsers,
+} = require("../Controllers/User");
 const router = Router();
 
-router.post("/register", async (req, res, next) => {
-  let { email, password ,isAdmin} = req.body;
-
-  try {
-    password = await bcrypt.hash(password, 13);
-    const newUser = await User.create({ email, password,isAdmin});
-    return res.send(newUser);
-  } catch (error) {
-    next(error);
-  }
-});
-
-
-router.post("/login", async (req, res, next) => {
-  let { email, password } = req.body;
-
-  try {
-    const userFound = await User.findByPk(email);
-    if (!userFound) {
-        return res.status(404).json({"message":"Email not found"})
-    }
-    const isValid = await bcrypt.compare(password,userFound.password)
-    if (!isValid) {
-        return res.status(400).json({"message": "Invalid Password"})
-    }
-    const accesToken = generateAccessToken(userFound)
-    return res.status(200).json({email:userFound.email,isAdmin:userFound.isAdmin,"accessToken":accesToken});
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/resetpassword/", resetPassword);
+router.get('/all',getAllUsers)
 
 module.exports = router;
